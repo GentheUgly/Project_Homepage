fc1a152fdfb95142d7eee15d9636855143a5c12d
GentheUgly <roy.faust.b@gmail.com>
2024-10-28 15:27:06 +0100

    multiplikation +dividierung

--- PowerPoint_Test/index.php
+++ PowerPoint_Test/index.php
@@ -5,13 +5,25 @@
     <meta name= viewport content= width=device-width initial-scale=1.0>
     <title> Mein Profil</title>
     <link rel="stylesheet" href="styles.css">
+    <script src="js.js"></script>
 </head>
 <body>
     <script>
-    var my_text = "Hello Js World";
-    var my_second_text = "it is my world";
-    window.alert(my_text + + my_second_text);
+        var my_text ="Hello Js World";
+        //var my_second_text ="it is my world";
+        var my_text = my_text + " it is my world";
+    
+        var my_int = 300;
+        my_int = my_int +100;
+    
+        //window.alert(my_text);
+    
+        my_int =my_int +" "+ my_text;
+    
+        window.alert(my_int);
+
     </script>
+           
     <header>
         <h1>Willkommen auf meiner Profilseite</h1>
         <nav>
@@ -20,14 +32,16 @@
             <a href="#contact">Kontakt</a>
         </nav>
     </header>
-
+<!-- comment-->
     <section id="about">
         <h2>Über mich</menu></h2>
         <p>Ich bin ein Webentwickler und lerne HTML und CSS.</p>
     </section>
 
+    <input id="my_input" types="text">
+
     <section id="projects">
-        <h2>Projekte</h2>
+        <h2>Projekte Header</h2>
         <div class="project">
             <h3>Projekt 1</h3>
             <p>Beschreibung des Projekts.</p>
---
+++ PowerPoint_Test/js.js
@@ -0,0 +1,12 @@
+function my_func(){
+    /*var my_text = "hello js world";
+    //var my_second_text ="it is ma world";
+    var my_text = my_text + " it is my world";
+    var my_int = 300;
+    my_int = my_int + 100;
+
+    my_int= my_int + my_text;
+
+
+    window.alert(my_int);
+ }
